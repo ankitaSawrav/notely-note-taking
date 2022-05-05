0b9690ec600bcbc71117545024eabc499c27088e
Ankita Sawrav <ankita.sawrav@gmail.com>
2022-05-05 22:59:24 +1000

    fixing js login user error and css checkbox

--- static/index.js
+++ static/index.js
@@ -1,17 +1,37 @@
+function ShowOnlyMyNotes() {
+navTags = document.getElementsByTagName('nav')
+console.log(navTags)
+const e = document.getElementsByClassName("user")
+console.log(e)
+    // childrenOfNavTag = navtag.children[0];
+    // console.log(childrenOfNavTag)
+    // if (childrenOfNavTag.hasAttribute('class')) {
+    //     console.log("found")
+        loggedInUserId = document.getElementsByClassName("user")[0]
+    //     console.log(loggedInUserId)
 
-loggedInUserId = document.getElementsByClassName("user")[0]
-userID = loggedInUserId.getAttribute("u-id")
-function ShowOnlyMyNotes(){
-    checkboxvalue = document.getElementById("check")
+        if (e && loggedInUserId.getAttribute("u-id")) {
+            userID = loggedInUserId.getAttribute("u-id")
+            console.log(userID)
+                checkboxvalue = document.getElementById("check")
+                console.log(checkboxvalue)
 
-    notes_elements = document.getElementsByClassName('notes')
-    for (note_element of notes_elements){
-        const elementUid =note_element.dataset['uid']
+                notes_elements = document.getElementsByClassName('notes')
+                for (note_element of notes_elements) {
+                    const elementUid = note_element.dataset['uid']
+                    console.log(elementUid)
+                    if(elementUid != userID){
+                        console.log(':<')
+                    }
 
-        if (checkboxvalue.checked == true && elementUid !== userID ){
-            note_element.style.display = "none";
-        }else {
-            note_element.removeAttribute("style");
+                    if (checkboxvalue.checked == true && elementUid !== userID) {
+                        
+                        note_element.style.display = "none";
+                    } else {
+                        note_element.removeAttribute("style");
+                    }
+                }
         }
-    }   
+        // }
+
 }
